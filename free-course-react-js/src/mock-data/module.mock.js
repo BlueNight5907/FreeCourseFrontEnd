4dad55f0f4afc618142b10308a0bc1401b52338c
HuyDepZai5907 <henrypoter22@gmail.com>
2022-06-15 22:58:16 +0700

    update views

---
+++ free-course-react-js/src/mock-data/module.mock.js
@@ -0,0 +1,42 @@
+export const modules = [
+  {
+    name: "Cấu trúc dữ liệu và giải thuật",
+    steps: [
+      {
+        href: "/",
+        name: "Đệ quy",
+        type: "video",
+      },
+      {
+        href: "/",
+        name: "Tham lam",
+        type: "video",
+      },
+      {
+        href: "/",
+        name: "Quy hoạch động",
+        type: "document",
+      },
+    ],
+  },
+  {
+    name: "Cấu trúc dữ liệu và giải thuật 2",
+    steps: [
+      {
+        href: "/",
+        name: "Đệ quy",
+        type: "video",
+      },
+      {
+        href: "/",
+        name: "Tham lam",
+        type: "video",
+      },
+      {
+        href: "/",
+        name: "Quy hoạch động",
+        type: "document",
+      },
+    ],
+  },
+];
